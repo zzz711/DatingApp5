24887a60581c771d1a842d0ddb243f7884d8c3a5
zzz711 <zzz8910@gmail.com>
2020-11-01 13:42:41 -0500

    Inital coomit -end of seciton 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<AppUser> Users { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
